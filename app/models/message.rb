f4684ab19b7de7d7c756b4d8fb41c32e27f39026
David Balatero <dbalatero@gmail.com>
2016-03-29 00:50:12 -0700

    Add #state to Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,8 +12,15 @@ class Message < ActiveRecord::Base
   validates :phone, length: { is: 10 }
 
   before_validation :format_phone_number
+  before_validation :ensure_state_is_capitalized
 
   private
+
+  def ensure_state_is_capitalized
+    return unless state.present?
+
+    self.state = self.state.upcase
+  end
 
   def format_phone_number
     return unless phone.present?
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -16,5 +16,20 @@ describe Message do
         expect { subject.valid? }.to_not raise_error
       end
     end
+
+    context "state validation" do
+      it "should always capitalize the state" do
+        subject.state = "wa"
+        subject.valid?
+
+        expect(subject.state).to eq("WA")
+      end
+
+      it 'should not blow up on nil' do
+        subject.state = nil
+
+        expect { subject.valid? }.to_not raise_error
+      end
+    end
   end
 end
