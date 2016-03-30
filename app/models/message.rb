0b201ba9cf6d9a80d97aaafc713eaa9df4e46af8
bion <bionjohnson@gmail.com>
2016-03-29 20:11:48 -0700

    Delete metaprogramming insanity

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,22 +10,6 @@ class Message < ActiveRecord::Base
     :contents,
     presence: true
 
-  def with_delegates(*delegates)
-    delegates = delegates.first if delegates.first.class == Array
-
-    delegates.each do |delegate|
-      self.define_singleton_method("include_#{delegate}") do
-        instance_variable_get("@#{delegate}")
-      end
-
-      self.define_singleton_method("include_#{delegate}=") do |val|
-        instance_variable_set("@#{delegate}", val)
-      end
-    end
-
-    self
-  end
-
   validates :phone, length: { is: 10 }
 
   before_validation :format_phone_number
