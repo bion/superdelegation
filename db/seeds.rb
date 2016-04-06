46a2d66db1a182be8be04604dfd78449837f0935
bion <bionjohnson@gmail.com>
2016-04-06 00:52:35 -0700

    Seeds raise exception if create fails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Delegate.find_or_create_by \
+Delegate.find_or_create_by! \
   state: 'WA',
   position: "Governor",
   name: "Jay Inslee",
   klass: "Delegates::WA::Inslee"
 
-Delegate.find_or_create_by \
+Delegate.find_or_create_by! \
   state: 'WA',
   position: "Senator",
   name: "Patty Murray",
   klass: "Delegates::WA::Murray"
 
-Delegate.find_or_create_by \
+Delegate.find_or_create_by! \
   state: 'WA',
   position: "Representative",
   name: "Rick Larsen",
