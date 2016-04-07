bd77d46e59a4c772d53bbb1309369a407fc53cd3
bion <bionjohnson@gmail.com>
2016-04-07 00:33:22 -0700

    Add Delegates::JSBase

---
+++ app/lib/delegates/js_base.rb
@@ -0,0 +1,29 @@
+class Delegates::JSBase
+  include Capybara::DSL
+
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+    configure_capybara
+  end
+
+  def deliver!
+    raise NotImplementedError
+  end
+
+  protected
+
+  def host
+    raise NotImplementedError
+  end
+
+  def configure_capybara
+    Capybara.configure do |config|
+      config.run_server = false
+      config.default_driver = :poltergeist
+      config.app = ""
+      config.app_host = host
+    end
+  end
+end
