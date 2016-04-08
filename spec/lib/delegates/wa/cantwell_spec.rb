4c16d0e2bf0a6d19b51950ca3ea6563d3b16a61e
bion <bionjohnson@gmail.com>
2016-04-08 11:03:29 -0700

    Tag cantwell_spec with :live_test

--- spec/lib/delegates/wa/cantwell_spec.rb
+++ spec/lib/delegates/wa/cantwell_spec.rb
@@ -1,38 +1,32 @@
-# CHANGE MESSAGE IF USING AGAIN!
-#
-# VCR cannot record phantomjs requests, so do not ever set this
-# to run during any kind of automated build system. Use sparingly
-# while developing as needed.
+require 'rails_helper'
 
-# require 'rails_helper'
+describe Delegates::WA::Cantwell, :live_test do
+  let(:message) do
+    instance_double "Message",
+      first_name: "Thomas",
+      last_name: "Shelby",
+      address1: "121 Fir St",
+      address2: "",
+      city: "Enumclaw",
+      zip: "98022",
+      email: "thommy22@yahoo.com",
+      phone: "2061345672",
+      contents: "Washington state needs fewer missile silos!"
+  end
 
-# describe Delegates::WA::Cantwell do
-#   let(:message) do
-#     instance_double "Message",
-#       first_name: "Thomas",
-#       last_name: "Shelby",
-#       address1: "121 Fir St",
-#       address2: "",
-#       city: "Enumclaw",
-#       zip: "98022",
-#       email: "thommy22@yahoo.com",
-#       phone: "2061345672",
-#       contents: "Washington state needs fewer missile silos!"
-#   end
+  subject { described_class.new(message) }
 
-#   subject { described_class.new(message) }
+  describe '#deliver!' do
+    it "should send a message to Maria Cantwell" do
+      expect(subject.deliver!).to eq(nil)
+    end
 
-#   describe '#deliver!' do
-#     it "should send a message to Maria Cantwell" do
-#       expect(subject.deliver!).to eq(nil)
-#     end
+    it "should handle failure by raising an error" do
+      allow(message).to receive(:city) { "" }
 
-#     it "should handle failure by raising an error" do
-#       allow(message).to receive(:city) { "" }
-
-#       expect {
-#         subject.deliver!
-#       }.to raise_error(Delegates::SubmissionError)
-#     end
-#   end
-# end
+      expect {
+        subject.deliver!
+      }.to raise_error(Delegates::SubmissionError)
+    end
+  end
+end
