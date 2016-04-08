f5e0e69896809ae14fe2a98231bf4bba51e0adc8
bion <bionjohnson@gmail.com>
2016-04-08 15:22:59 -0700

    Add heap analytics for development env

--- app/views/shared/_analytics.html.rb
+++ app/views/shared/_analytics.html.rb
@@ -1,7 +1,5 @@
 class Views::Shared::Analytics < Views::Base
   def content
-    return unless Rails.env.production?
-
     google_analytics
     heap_analytics
   end
@@ -9,6 +7,8 @@ class Views::Shared::Analytics < Views::Base
   private
 
   def google_analytics
+    return unless Rails.env.production?
+
     javascript <<-JS
       (function(i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
@@ -33,10 +33,18 @@ class Views::Shared::Analytics < Views::Base
   end
 
   def heap_analytics
-    javascript <<-JS
-      window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+    if Rails.env.production?
+      javascript <<-JS
+        window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
 
-      heap.load("3220453361");
-    JS
+        heap.load("3220453361");
+      JS
+    elsif Rails.env.development?
+      javascript <<-JS
+        window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+
+        heap.load("3648510825");
+      JS
+    end
   end
 end
