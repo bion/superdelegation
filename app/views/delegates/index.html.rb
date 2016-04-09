7688415d348030ade19b6ee7189933bc840e6671
bion <bionjohnson@gmail.com>
2016-04-08 19:23:36 -0700

    Two columns for message page

--- app/views/delegates/index.html.rb
+++ app/views/delegates/index.html.rb
@@ -1,76 +1,89 @@
 class Views::Delegates::Index < Views::Base
-  needs :message, :delegates
+  needs :message, :delegates, :state
 
   def content
-    full_row do
-      h2 "Bernie Superdelegation"
+    full_row_left do
+      h2 "Superdelegation"
       h4 "Tell your superdelegates to respect your vote"
     end
 
-    full_row(class: 'description') do
+    full_row_left do
       p <<-TEXT
-        Now that Bernie has won Washington by a landslide, it's important we
+        Now that Bernie has won #{state.name}, it's important we
         pressure our superdelegates to back him as well. Superdelegates
         represent approximately 10,000 votes worth of power, so it's well
         worth the time. Superdelegation allows you to easily send the same
         message to the superdelegates in your state.
-        TEXT
+      TEXT
     end
 
     error_messages
 
     form_for :message, url: delegates_path(params[:state]), method: :post do |f|
-      full_row do
-        h4 "Statewide Delegates"
+      div(class: 'columns large-6') do
+        full_row_left do
+          h4 "Statewide Delegates"
+        end
+
+        other_delegate_inputs(f)
+
+        full_row_left do
+          h4(class: "rep-header") { text "Representatives" }
+          p do
+            text "House Representatives only accept messages from people in their districts. "
+            link_to 'Find your rep',
+              'http://www.house.gov/representatives/find/',
+              target: '_blank'
+          end
+        end
+
+        representative_inputs(f)
       end
 
-      other_delegate_inputs(f)
+      div(class: 'columns large-6') do
+        message_fields(f)
 
-      full_row do
-        h4(class: "rep-header") { text "Representatives" }
-        p "House Representatives only accept messages from people in their districts"
+        full_row_left(classes: %w[recaptcha-container]) do
+          text recaptcha_tags(stoken: false)
+        end
+
+        full_row_left { f.submit "Send Message", class: 'button' }
       end
-
-      representative_inputs(f)
-      message_fields(f)
-
-      full_row(class: 'recaptcha-container') { text recaptcha_tags(stoken: false) }
-      full_row { f.submit "Send Message", class: 'button' }
     end
   end
 
   private
 
   def message_fields(f)
-    full_row do
+    full_row_left do
       f.label :contents do
         text 'Your Message'
         f.text_area :contents, rows: 6
       end
     end
 
-    full_row do
+    full_row_left do
       f.label :first_name do
         text 'First Name'
         f.text_field :first_name
       end
     end
 
-    full_row do
+    full_row_left do
       f.label :last_name do
         text 'Last Name'
         f.text_field :last_name
       end
     end
 
-    full_row do
+    full_row_left do
       f.label :address1 do
         text 'Address Line 1'
         f.text_field :address1
       end
     end
 
-    full_row do
+    full_row_left do
       f.label :address2 do
         text 'Address Line 2'
         f.text_field :address2
@@ -91,14 +104,14 @@ class Views::Delegates::Index < Views::Base
       <<-MSG
         +4 Zip Extension (required for contacting some officials, #{zip_finder_link})
       MSG
-        .html_safe
+    .html_safe
 
     field_in_row(f, :email)
     field_in_row(f, :phone)
   end
 
   def field_in_row(f, attr, label_text = nil)
-    full_row do
+    full_row_left do
       f.label attr do
         text label_text || attr.to_s.titleize
         f.text_field attr
@@ -130,7 +143,7 @@ class Views::Delegates::Index < Views::Base
     delegate_title = delegate.name.titleize
     el_name = "message[delegate_ids][]"
 
-    full_row do
+    full_row_left do
       p(class: "delegate-name") do
         text "Send to #{delegate.position.titleize} #{delegate_title}"
       end
