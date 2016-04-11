adfdd920a3f2ff6a48e7cd27ed3bf4d116fedd07
David Balatero <dbalatero@gmail.com>
2016-04-11 10:13:49 -0700

    Add delayed_job web interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'landing#index'
+
   get 'privacy', to: 'landing#privacy'
   get ':state', to: 'delegates#index', as: 'state'
+
+  match '/admin/delayed_job' => DelayedJobWeb,
+    anchor: false,
+    via: [:get, :post]
 
   scope ':state' do
     resources :delegates, only: [:create]
