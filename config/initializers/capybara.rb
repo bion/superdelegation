e1c450b321d0512350a96c84df3d173aced3b616
bion <bionjohnson@gmail.com>
2016-04-12 19:22:53 -0700

    Add configuration

---
+++ config/initializers/capybara.rb
@@ -0,0 +1,12 @@
+if Rails.application.config.x.proxy.present?
+  proxy_config = Rails.application.config.x.proxy
+
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new \
+      app,
+      phantomjs_options: [
+        "--proxy=#{proxy_config.host}:#{proxy_config.port}",
+        "--proxy-auth=#{proxy_config.username}:#{proxy_config.password}"
+      ]
+  end
+end
